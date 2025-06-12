5384dd69604f9808242ee16e02b9961b5b627843
archanasondur <aksondur@gmail.com>
2025-06-12 16:40:43 -0700

    Remove test.py after initial testing

--- test.py
+++
@@ -1,7 +0,0 @@
-from yelp_api import search_restaurants
-
-results = search_restaurants("Los Angeles", food_type="tacos", price_level="1,2")
-
-for place in results:
-    print(f"{place['name']} - {place['rating']}⭐")
-    print(place['location']['address1'], "\n")
